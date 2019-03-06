ccc7e3341d64f2e790b6b8124b1d2c9d3ef181c9
Nikunj Joshi PHP Developer <38153522+nikunjjoshiphpdeveloper@users.noreply.github.com>
2019-03-07 00:15:06 +0530

    city_list.php
    City List PHP file

---
+++ autocomplete/city_list.php
@@ -0,0 +1,10 @@
+<?php
+	$cities = array();
+	$cities[] = "Ahmedabad";
+	$cities[] = "Bangalore";
+	$cities[] = "Jammu & Kashmir";
+	$cities[] = "Kerala";
+	$cities[] = "Ladakh";
+	
+	echo json_encode($cities);
+?>
